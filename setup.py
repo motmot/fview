6d3a984be94d93336035573c9f3044109c2ae1f8
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2007-12-29 18:19:53 +0000

    update debian packaging info
    git-svn-id: https://code.astraw.com/motmot/trunk/fview@358 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
 
-import os, sys
-
 setup(name='motmot.fview',
       description='extensible camera viewer program (part of the motmot camera packages)',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       url='http://code.astraw.com/projects/motmot',
       license='BSD',
-      version='0.5.1', # keep in sync with fview/version.py
+      version='0.5.1', # keep in sync with motmot/fview/version.py
       zip_safe=True,
       namespace_packages = ['motmot'],
       packages = find_packages(),
