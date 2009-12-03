5f996fccec494f9f77713a5d7f070acb09aa2655
Andrew Straw <strawman@astraw.com>
2009-12-03 13:10:08 -0800

    remove old cruft files

--- setup.py
+++ setup.py
@@ -14,24 +14,8 @@ setup(name='motmot.fview',
                                       'fview_fmf_replay = motmot.fview.fview_fmf_replay:main',
                                       ]},
       package_data = {'motmot.fview':['fview.xrc','fview.gif',
-                                      # ImperX .xml files for config (used on Windows only)
-                                      'IPX-2M30-G.xml',
-                                      'IPX-2M30-L.xml',
-                                      # camera .conf files for camwire (used on linux only)
-                                      'A602f.conf',
-                                      'A622f.conf',
-                                      'Scorpion.conf',
-                                      'Fire-i BCL 1.2.conf',
-                                      'Fire-i BBW 1.3.conf',
                                       'fview_icon2.png', # for .desktop icon
                                       ]},
       eager_resources = ['motmot/fview/fview.xrc','motmot/fview/fview.gif',  # unpack files together
-                         'motmot/fview/IPX-2M30-G.xml',
-                         'motmot/fview/IPX-2M30-L.xml',
-                         'motmot/fview/A602f.conf',
-                         'motmot/fview/A622f.conf',
-                         'motmot/fview/Scorpion.conf',
-                         'motmot/fview/Fire-i BCL 1.2.conf',
-                         'motmot/fview/Fire-i BBW 1.3.conf',
                          ],
       )
